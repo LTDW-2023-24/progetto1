5a3940895f0918a41324b74a04c8d6e9422b9dfb
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2024-05-28 15:08:31 +0200

    auth.inc.php

--- content-add2.php
+++ content-add2.php
@@ -1,8 +1,6 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    session_start();
 
     require "include/config.inc.php";
     require "include/dbms.inc.php";
@@ -37,7 +35,6 @@ error_reporting(E_ALL);
 
     }
     
-
     $main->setContent("body", $form->emit());
     $main->close();
 
--- include/auth.inc.php
+++ include/auth.inc.php
@@ -3,27 +3,66 @@
     $config['auth'] = "true";
 
 
-    $passwd = md5("{$_POST['password']}|".md5($_POST['username']));
-    
-    $result = $mysqli->query("SELECT username, name, surname, email FROM `user` WHERE username = '{$_POST['username']}' AND password = '{$passwd}'");
+    if (!isset($_SESSION['user'])) {
 
-    if (!$result) {
-        die("Error!");
-    } else {
+        $passwd = md5("{$_POST['password']}|".md5($_POST['username']));
+        
+        $result = $mysqli->query("SELECT username, name, surname, email FROM `user` WHERE username = '{$_POST['username']}' AND password = '{$passwd}'");
 
-        if ($result->num_rows == 1) {
-            $user = $result->fetch_array();\
-            $_SESSION['user'] = $user;
-
+        if (!$result) {
+            die("Error!");
         } else {
 
-            // utente non esiste
+            if ($result->num_rows == 1) {
+                $user = $result->fetch_array();
+                $_SESSION['user'] = $user;
 
-            Header("Location: error.php?error=001");
-            exit;
+                $result = $mysqli->query("
+                    SELECT service.script 
+                    FROM user_role
+                    LEFT JOIN role_service
+                    ON role_service.role_id = user_role.role_id
+                    LEFT JOIN service
+                    ON service.id = role_service.service_id
+                    WHERE user_role.username = '{$_POST['username']}'
+                ");
+
+                if (!$result) {
+                    die("Error!");
+                } else {
+                    while ($data = $result->fetch_array()) {
+                        $service[$data['script']] = true;
+                    }
+
+                    $_SESSION['user']['service'] = $service;
+                
+                }
+
+            } else {
+
+                // utente non esiste
+
+                Header("Location: error.php?error=001");
+                exit;
+            }
+
+
         }
+    } 
 
+    /* user exists and passwd is correct */
+
+
+    $script = basename($_SERVER['SCRIPT_NAME']);
+    if (!isset($_SESSION['user']['service'][$script])) {
+
+        // user does not have permission for this script
+        
+        Header("Location: error.php?error=002");
+        exit;
 
     }
+
+    
 
 ?>
